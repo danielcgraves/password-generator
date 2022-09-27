977d1a38e8df9b838d6bc2bfa174ba682c74c1a0
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2022-09-27 14:14:21 -0600

    created a for loop with math methods that generates the random password

--- script.js
+++ script.js
@@ -5,40 +5,41 @@
 
 function generatePassword() {
   var passwordCharacters = [];
+  
   var passwordLength = parseInt(prompt("Choose a password length (must be between 8 and 128)"));
     if (passwordLength > 128 || passwordLength < 8 || isNaN(passwordLength)) {
       alert("Please enter a number between 8 and 128");
-      return false;
-    }
+      return "Try Again"
+    } ;
+  
   var incLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
     if (confirm("Would you like to include lowercase characters?")){
       passwordCharacters = passwordCharacters.concat(incLowerCase);
     }
+  
   var incUpperCase = incLowerCase.map(element => {
     return element.toUpperCase();
   });
     if (confirm("Would you like to include uppercase characters?")){
       passwordCharacters = passwordCharacters.concat(incUpperCase);
     }
+  
   var incNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     if (confirm("Would you like to include numbers?")){
       passwordCharacters = passwordCharacters.concat(incNum);
     }
+  
   var incSpecialChar = ["!", `"`, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
     if (confirm("Would you like to include special characters?")){
       passwordCharacters = passwordCharacters.concat(incSpecialChar);
     }
-
-    console.log(passwordCharacters);
-//4. Validate the input and at least one character type should be selected
-
-
-//5. Generate password (meets accepted criteria)
-
-
-//6. Password is either generated to page or alerted.
-
-  return "hola";
+  
+  var securePassword = "";
+  for (var i = 0; i < passwordLength; i++){
+    var randomCharacters = Math.floor(Math.random() * passwordCharacters.length);
+    securePassword = securePassword + passwordCharacters[randomCharacters];
+  }
+  return securePassword;
 }
 
 // Get references to the #generate element
