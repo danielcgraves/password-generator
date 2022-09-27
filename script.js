4ffa143a4defc7956380a336fd012bf086f5fd70
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2022-09-27 11:45:30 -0600

    created prompt and confirms for variables inside generatePassword function

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 // Assignment code here
+
+
+
 function generatePassword(){
-  console.log("Button clicked")
+  var passwordLength = prompt("Choose a password length (must be between 8 and 128)")
+    if (passwordLength > 128) {
+      console.log()
+    } else if (passwordLength < 8){
+      console.log("Please enter a number between 8 and 128")
+    } else  {
+      console.log(passwordLength)
+       }
+  
+  var incLowercase = confirm("Would you like to include lowercase characters?")
+    console.log(incLowercase);
+
+  var incUppercase = confirm("Would you like to include lowercase characters?")
+    console.log(incUppercase);
+  
+  var incNum = confirm("Would you like to include lowercase characters?")
+  console.log(incNum);
+
+  var incSpecialChar = confirm("Would you like to include lowercase characters?")
+  console.log(incSpecialChar);
+
+
+
+ /*  confirm("Would you like to include uppercase characters?")
+  confirm("Would you like to include numbers?")
+  confirm("Would you like to include special characters?") */
+
+  //Pseudocode
+//1. create prompts (steps 2 and 3) x
+//2. create a password length between 8 - 128 
+//3. confirm whether or not to include lowercase, uppercase, numbers, special chracters
+
+
+//4. Validate the input and at least one character type should be selected
+
+
+//5. Generate password (meets accepted criteria)
+
+
+//6. Password is either generated to page or alerted.
+
   return "hola";
 }
 
